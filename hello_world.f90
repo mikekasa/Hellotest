fd5ef9c623bcdeb1855b5962bad44c70f643a0d5
mikekasa <kasahara@matsuo-no-MacBook-Air.local>
2016-11-28 21:44:36 +0900

    [add] 1から10までの和を出す計算追加
    ∑axの行を追加しました。

--- hello_world.f90
+++ hello_world.f90
@@ -1,6 +1,15 @@
 program test
   implicit none
+  real(8) :: ax, ay
+  integer :: j,k,l 
   
   write(6,*) "Hello world!!!"
+  ax = 0.d0 
+  ay = 0.d0
+  do j = 1, 10
+    ax = ax + ay
+    ay = ay + j
+  end do
+  write(6,*) "SUM(1:10)", ay
 
 end program
